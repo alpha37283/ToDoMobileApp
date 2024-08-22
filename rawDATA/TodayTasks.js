a5ab155480ff75a47f31ed11dc80cb36e8a7c0b9
Muneeb Shah <codewithmuneeb7@gmail.com>
2024-08-22 23:43:21 +0500

    done navigation to the OTP page

--- rawDATA/TodayTasks.js
+++ rawDATA/TodayTasks.js
@@ -6,14 +6,14 @@ const TodayTasks = [{
 },
 {
     id : 102,
-    title : 'Going to Bed',
-    desc : 'hehe boy, nigga money no money, need money oh fuck no money bohut benchod hy ye youtube wale itne ads dete hain ',
+    title : 'React native Project',
+    desc : 'Starting TS ',
     time : '10:00'
 },
 {
     id : 103,
-    title : 'Wash Something go down go to hell ',
-    desc : 'i dont knwo what i am doing at 3:00 but it is good',
+    title : 'DO something!!! ',
+    desc : 'ok',
     time : '10:00'
 }
 ]
